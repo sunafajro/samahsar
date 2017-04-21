7068da4c5f8516b88086f9b0cba4ddb1a014d467
Evgeny Belkin <sunafajro@gmail.com>
2017-04-21 20:10:21 +0300

    добавляем локализацию

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Lang from 'vuejs-localization'
 import VueResource from 'vue-resource'
 import VueCookies from 'vue-cookies'
 import App from './App'
 import router from './router'
 
+Lang.requireAll(require.context('./lang', true, /\.js$/));
+
+Vue.use(Lang);
 Vue.use(VueResource);
 Vue.use(VueCookies);
 
@@ -18,6 +22,7 @@ new Vue({
   template: '<App/>',
   components: { App },
   created () {
+  	    this.$lang.setLang('ru')
   	    if(!this.$cookies.isKey('lastterms')) {
             this.$cookies.set('lastterms', '', 60*60*24, '/');
         }
